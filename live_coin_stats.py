29418775c63b87203b143ef0cd82fd6983e1a5c4
cosmocat27 <cosmocat27@gmail.com>
2018-01-09 20:19:37 -0500

    fix bugs and add docs

--- live_coin_stats.py
+++ live_coin_stats.py
@@ -8,17 +8,25 @@ Automatically reads in coin stats data from coinmarketcap.com
 and updates an excel file
 """
 
-import json, requests, openpyxl
+import json, requests, openpyxl, sys
+from datetime import datetime
 
 
-url = "https://api.coinmarketcap.com/v1/ticker/"
+limit = sys.argv[1] if len(sys.argv) > 1 else 500
+now = datetime.now()
+
+url = "https://api.coinmarketcap.com/v1/ticker/?limit={}".format(limit)
+print("fetching data from {}".format(url))
+
 response = requests.get(url)
 response.raise_for_status()
 
 data_json = json.loads(response.text)
 
-wb = openpyxl.load_workbook("crypto_hist.xlsx")
+wb = openpyxl.load_workbook("crypto_update.xlsx")
 ws = wb.get_sheet_by_name("current_prices")
+
+ws['O1'] = now.strftime("%Y-%m-%d %H:%M")
 
 for rank, coin in enumerate(data_json):
     ws.cell(row=rank+2, column=1).value = int(coin['rank'])
@@ -27,12 +35,16 @@ for rank, coin in enumerate(data_json):
     ws.cell(row=rank+2, column=4).value = round(float(coin['price_usd']), 2)
     ws.cell(row=rank+2, column=5).value = round(float(coin['market_cap_usd']), 2)
     ws.cell(row=rank+2, column=6).value = round(float(coin['24h_volume_usd']), 2)
-    ws.cell(row=rank+2, column=7).value = round(float(coin['percent_change_1h'])/100, 4)
-    ws.cell(row=rank+2, column=8).value = round(float(coin['percent_change_24h'])/100, 4)
-    ws.cell(row=rank+2, column=9).value = round(float(coin['percent_change_7d'])/100, 4)
+    ws.cell(row=rank+2, column=7).value = round(float(coin['percent_change_1h'])/100, 4) \
+        if coin['percent_change_1h'] else ""
+    ws.cell(row=rank+2, column=8).value = round(float(coin['percent_change_24h'])/100, 4) \
+        if coin['percent_change_24h'] else ""
+    ws.cell(row=rank+2, column=9).value = round(float(coin['percent_change_7d'])/100, 4) \
+        if coin['percent_change_7d'] else ""
     ws.cell(row=rank+2, column=10).value = round(float(coin['available_supply']))
     ws.cell(row=rank+2, column=11).value = round(float(coin['total_supply']))
     ws.cell(row=rank+2, column=12).value = round(float(coin['max_supply'])) \
         if coin['max_supply'] else ""
 
-wb.save("crypto_hist.xlsx")
+print(now.strftime("complete: %Y-%m-%d %H:%M"))
+wb.save("crypto_update.xlsx")
--- parse_coin_hist.py
+++ parse_coin_hist.py
@@ -3,12 +3,23 @@
 Created on Mon Jan  8 16:34:20 2018
 
 @author: cosmo
+
+Program to pull historical price data from an html page, given either the url
+or the html file, and saves the data to an excel file. Also able to read a
+list of coin names and urls from text file and pull for each.
+
+E.g.
+[coin name 1] [url 1]
+etc.
+
+run:
+python parse_coin_hist.py [url/filename] [coin_name]
 """
 
 import requests, openpyxl, sys
 
 
-def parse_hist(url = None, filename = None, coin_name = "new coin"):
+def parse_hist(wb, url = None, filename = None, coin_name = "new coin"):
     if url is None and filename is None:
         return "missing source!"
     
@@ -24,14 +35,13 @@ def parse_hist(url = None, filename = None, coin_name = "new coin"):
         with open(filename) as source:
             lines = source.readlines()
     
-    wb = openpyxl.load_workbook("crypto_hist.xlsx")
     if coin_name not in wb.get_sheet_names():
         wb.create_sheet(index=len(wb.get_sheet_names()), title=coin_name)
     ws = wb.get_sheet_by_name(coin_name)
     
     start_read = 0
     curr_row = 0
-    curr_col = 0
+    curr_col = 1
     
     for line in lines:
         if "<table" in line:
@@ -41,6 +51,8 @@ def parse_hist(url = None, filename = None, coin_name = "new coin"):
             continue
         if "<tr" in line:
             curr_row += 1
+            if curr_row > 1:
+                ws.cell(row=curr_row, column=curr_col).value = curr_row-1
             #print("new row")
         if ("<th" in line and "<thead" not in line):
             curr_col += 1
@@ -48,21 +60,22 @@ def parse_hist(url = None, filename = None, coin_name = "new coin"):
             pos1 = line[pos0:].find(">") + pos0
             pos2 = line[pos1:].find("<") + pos1
             ws.cell(row=curr_row, column=curr_col).value = line[pos1+1:pos2]
-            #print(pos0, pos1, pos2)
-            #print(line, line[pos1+1:pos2])
         if "<td" in line:
             curr_col += 1
             pos0 = line.find("<td")
             pos1 = line[pos0:].find(">") + pos0
             pos2 = line[pos1:].find("<") + pos1
-            ws.cell(row=curr_row, column=curr_col).value = line[pos1+1:pos2]
-            #print(pos0, pos1, pos2)
             #print(line, line[pos1+1:pos2])
+            if "-" in line[pos1+1:pos2]:
+                ws.cell(row=curr_row, column=curr_col).value = 0
+            elif " " in line[pos1+1:pos2]:
+                ws.cell(row=curr_row, column=curr_col).value = line[pos1+1:pos2]
+            else:
+                ws.cell(row=curr_row, column=curr_col).value = float("".join(line[pos1+1:pos2].split(',')))
         if "</tr" in line:
-            curr_col = 0
+            curr_col = 1
             #print("end row")
     
-    wb.save("crypto_hist.xlsx")
     return True
 
 
@@ -83,11 +96,17 @@ if __name__ == '__main__':
         coin_name = sys.argv[2]
         url_list = 0
     
+    wb = openpyxl.load_workbook("crypto_update.xlsx")
+    
     if url_list == 1:
         with open(filename) as source:
             lines = source.readlines()
             for line in lines:
+                if len(line.split(" ")) < 2:
+                    continue
                 coin_name, url = line.split(" ")
-                print(coin_name, parse_hist(url, None, coin_name))
+                print(coin_name, parse_hist(wb, url, None, coin_name))
     else:
-        print(coin_name, parse_hist(url, filename, coin_name))
+        print(coin_name, parse_hist(wb, url, filename, coin_name))
+    
+    wb.save("crypto_update.xlsx")
